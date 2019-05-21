4fbcec6ea0e6f9b6aa61a3811bc93a12b7458264
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-05-20 23:13:10 -0300

    Adding initial code

--- consumer.py
+++ consumer.py
@@ -1,1 +1,21 @@
-print("consumer")
+from kafka import KafkaConsumer
+import twitter
+
+# https://developer.twitter.com/en/apps
+
+
+class Twitter:
+    def __init__(self, consumer_key=None, consumer_secret=None, access_token=None, access_token_secret=None)
+    self.consumer_key = consumer_key
+    self.consumer_secret = consumer_secret
+    self.access_token = access_token
+    self.access_token_secret = access_token_secret
+
+    def authenticate(self):
+        api = twitter.Api(consumer_key=self.consumer_key, consumer_secret=self.consumer_secret,
+                          access_token_key=self.access_token, access_token_secret=self.access_token_secret)
+        return api
+
+    def getTimeline(self):
+        for tweet in authenticate().GetHomeTimeline():
+            print(tweet.text)
--- producer.py
+++ producer.py
@@ -1,1 +1,5 @@
-print("producer")
+from kafka import KafkaProducer
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+future = producer.send('topic', b'test')
+result = future.get(timeout=60)
