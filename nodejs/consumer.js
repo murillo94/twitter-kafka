38f31bc45af1687878f7230f6e8417de12da8ada
MURILLO DE MIRANDA PEREIRA <murillo.pereira@catolicasc.edu.br>
2019-05-29 19:33:33 -0300

    Writting data consumer in a single file

--- nodejs/consumer.js
+++ nodejs/consumer.js
@@ -1,6 +1,7 @@
 const ConfigParser = require('configparser');
 const language = require('@google-cloud/language');
 const { SimpleConsumer } = require('no-kafka');
+const fs = require('fs');
 
 const config = new ConfigParser();
 config.read('../config.cfg');
@@ -15,8 +16,11 @@ GOOGLE_APPLICATION_CREDENTIALS = config.get(
 process.env['GOOGLE_APPLICATION_CREDENTIALS'] = GOOGLE_APPLICATION_CREDENTIALS;
 
 const clientGoogle = new language.LanguageServiceClient();
+const dir = './records';
 
 function initKafka() {
+  createFolder();
+
   const consumer = new SimpleConsumer({
     connectionString: `localhost:${KAFKA_ENDPOINT_PORT}`
   });
@@ -26,7 +30,7 @@ function initKafka() {
       const value = message.value.toString('utf8');
       const { text } = JSON.parse(value);
 
-      if (text) await analyzeText(text);
+      if (text) await analyzeText(text)
     });
   };
 
@@ -43,10 +47,22 @@ async function analyzeText(text) {
 
   const [result] = await clientGoogle.analyzeSentiment({ document });
   const { score, magnitude } = result.documentSentiment;
+  const data = JSON.stringify({text, score, magnitude})
 
-  console.log(`Text: ${text}`);
-  console.log(`Score: ${score}`);
-  console.log(`Magnitude: ${magnitude}\n`);
+  createFile(data)
+}
+
+function createFolder() {
+  if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+  }
+}
+
+function createFile(data) {
+  fs.writeFile(`${dir}/twitter-${Date.now()}.txt`, data, (err) => {
+    if (err) console.log(err);
+    console.log("Successfully written to file.");
+  });
 }
 
 initKafka();
